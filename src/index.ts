290cfd3f66e5ab7e45a7fad4cd65ce9880142a4c
simran <simranag951@gmail.com>
2024-03-22 11:21:07 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express,{Request, Response} from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoutes"
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>
+console.log("Connected to Database"))
+
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+app.get("/test", async (req: Request, res:Response ) => {
+    res.json({message: "Hello!!!!!"});
+});
+
+app.get("/health", async (req:Request, res:Response)=>{
+    res.send({message: "health OK!"});
+});
+
+app.use("/api/my/user", myUserRoute)
+
+app.listen(7000, ()=>{
+    console.log("server started on localhost:7000");
+})
---
+++ src/routes/MyUserRoutes.ts
@@ -0,0 +1,14 @@
+import { validateMyUserRequest } from './../middleware/validation';
+import express from "express";
+import MyUserController from "../controllers/MyUserController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+
+
+const router = express.Router();
+
+// /api/my/user
+router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
+router.post("/",jwtCheck, MyUserController.createCurrentUser);
+router.put("/",jwtCheck,jwtParse,validateMyUserRequest,MyUserController.updateCurrentUser);
+
+export default router;
