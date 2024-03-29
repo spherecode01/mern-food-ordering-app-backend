8be34c1b82910a78181f49eb68e4cabf98b7b602
simran <simranag951@gmail.com>
2024-03-29 16:00:19 +0530

    added stripe

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoutes";
 import {v2 as cloudinary} from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import restaurantRoute from './routes/RestaurantRoute';
+import orderRoute from "./routes/OrderRoute"
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>
 console.log("Connected to Database"));
@@ -16,8 +18,12 @@ cloudinary.config({
 })
 
 const app = express();
-app.use(express.json())
-app.use(cors())
+
+app.use(cors());
+
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+
+app.use(express.json());
 
 app.get("/test", async (req: Request, res:Response ) => {
     res.json({message: "Hello!!!!!"});
@@ -29,6 +35,8 @@ app.get("/health", async (req:Request, res:Response)=>{
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute);
 
 app.listen(7000, ()=>{
     console.log("server started on localhost:7000");
